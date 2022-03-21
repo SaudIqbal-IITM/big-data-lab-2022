b4fa2be6977d78e6536694699e49935d4d641b58
Saud Iqbal <me18b169@smail.iitm.ac.in>
2022-03-21 05:48:35 +0000

    lab-6 first commit

---
+++ labs/lab_6/gcfs/main.py
@@ -0,0 +1,30 @@
+def pub_filename(data, context):
+	"""
+		Background Cloud Function to be triggered by Cloud Storage.
+		This generic function logs relevant data when a file is changed.
+		Args:
+			data (dict): The Cloud Functions event payload.
+			context (google.cloud.functions.Context): Metadata of triggering event.
+		Returns:
+			None; the output is written to Stackdriver Logging
+	"""
+
+	## Setup.
+	# Imports.
+	from google.cloud import pubsub_v1
+
+	# Client (Pub/Sub publisher).
+	publisher = pubsub_v1.PublisherClient()
+
+	# Topic name.
+ 	topic_name = "projects/bdl-2022-341111/topics/lab-6"
+
+	## Logic.
+	# Filename.
+	filename = data["name"]
+
+	# Publish the filename.
+	future = publisher.publish(topic_name, bytes(filename, "utf-8"))
+
+	# Wait.
+	future.result()
---
+++ labs/lab_6/script_q_1_c.py
@@ -0,0 +1,44 @@
+## Setup.
+# Imports.
+import base64
+from google.cloud import pubsub_v1
+from google.cloud import storage
+
+# Client (GCS).
+client = storage.Client()
+
+# Bucket.
+bucket = client.get_bucket("me18b169_bdl_2022_bucket")
+
+# Client (Pub/Sub subscriber).
+subscriber = pubsub_v1.SubscriberClient()
+
+# Topic name.
+topic_name = "projects/bdl-2022-341111/topics/lab-6"
+
+# Subscription name.
+subscription_name = "projects/bdl-2022-341111/subscriptions/lab-6-sub"
+
+## Logic.
+# Callback.
+def callback(message):
+	filename = base64.b64decode(message["data"]).decode("utf-8")
+
+	blob = bucket.get_blob(filename)
+
+	data = blob.download_as_string().decode('utf-8')
+
+	num_lines = len(data.split('\n'))
+
+	print(f"Number of lines in the file, {filename}: {num_lines}")
+
+	message.ack()
+
+# Subscribe.
+future = subscriber.subscribe(subscription_name, callback)
+
+# Wait.
+try:
+    future.result()
+except KeyboardInterrupt:
+    future.cancel()
