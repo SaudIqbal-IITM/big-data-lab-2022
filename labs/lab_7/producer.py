52e1023daf1d2220dad3f3d69457ac2ececa13e2
Saud Iqbal <me18b169@smail.iitm.ac.in>
2022-04-01 17:32:11 +0000

    lab-7 first commit

---
+++ labs/lab_7/producer.py
@@ -0,0 +1,27 @@
+## Setup.
+# Imports.
+from kafka import KafkaProducer
+import csv
+from google.cloud import storage
+
+# Client (GCS).
+client = storage.Client()
+
+# Bucket.
+bucket = client.get_bucket("me18b169_bdl_2022_bucket")
+
+# Kafka producer.
+producer = KafkaProducer(bootstrap_servers='127.0.0.1:9092'
+
+## Logic.
+# Download data file from GCS.
+blob = bucket.get_blob("iris.data")
+data = blob.download_as_string()
+
+# Write data row by row to Kafka topic.
+for entry in data.split('\n')[1:]:
+	# Send.
+	producer.send("lab-7", entry)
+
+	# Wait.
+	producer.flush()
