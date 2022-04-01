3a4ac8fd263d3cad83ef6ee2a29f24668cf7f0e0
Saud Iqbal <me18b169@smail.iitm.ac.in>
2022-04-01 18:26:25 +0000

    bug fix producer.py

--- labs/lab_7/producer.py
+++ labs/lab_7/producer.py
@@ -16,7 +16,7 @@ producer = KafkaProducer(bootstrap_servers='127.0.0.1:9092'
 ## Logic.
 # Download data file from GCS.
 blob = bucket.get_blob("iris.data")
-data = blob.download_as_string()
+data = blob.download_as_string().decode('utf-8')
 
 # Write data row by row to Kafka topic.
 for entry in data.split('\n')[1:]:
