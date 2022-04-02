136be5b11934695063660bc043ff5cd3dcbac0e9
Saud Iqbal <me18b169@smail.iitm.ac.in>
2022-04-02 14:48:29 +0000

    minor fix

--- labs/lab_7/producer.py
+++ labs/lab_7/producer.py
@@ -1,7 +1,6 @@
 ## Setup.
 # Imports.
 from kafka import KafkaProducer
-import csv
 from google.cloud import storage
 
 # Client (GCS).
@@ -11,12 +10,12 @@ client = storage.Client()
 bucket = client.get_bucket("me18b169_bdl_2022_bucket")
 
 # Kafka producer.
-producer = KafkaProducer(bootstrap_servers='127.0.0.1:9092'
+producer = KafkaProducer(bootstrap_servers="127.0.0.1:9092")
 
 ## Logic.
 # Download data file from GCS.
 blob = bucket.get_blob("iris.data")
-data = blob.download_as_string().decode('utf-8')
+data = blob.download_as_string().decode("utf-8")
 
 # Write data row by row to Kafka topic.
 for entry in data.split('\n')[1:]:
